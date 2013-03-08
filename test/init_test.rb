bc51bd9c6ebf991fd2255f7a582621feac0863c9
Yossef Mendelssohn <ymendel@pobox.com>
2013-03-08 01:00:09 -0800

    let conditions come from args or options hash

--- test/init_test.rb
+++ test/init_test.rb
@@ -87,3 +87,52 @@ class InitializationTestWithCustomWhereClause < ModelIterator::TestCase
   end
 end
 
+class InitializationTestWithCustomWhereClauseInOptions < ModelIterator::TestCase
+  def setup
+    @iter = ModelIterator.new Model,
+      :redis => RedisClient.new, :start_id => 5, :limit => 10,
+      :conditions => ['public = ?', true]
+  end
+
+  def test_sets_klass
+    assert_equal Model, @iter.klass
+  end
+
+  def test_sets_current_id
+    assert_equal 5, @iter.current_id
+  end
+
+  def test_sets_conditions
+    assert_equal ['models.id > ? AND (public = ?)', @iter.current_id, true], @iter.conditions
+  end
+
+  def test_sets_limit
+    assert_equal 10, @iter.limit
+  end
+end
+
+class InitializationTestWithCustomWhereClauseInOptionsOverridingArguments < ModelIterator::TestCase
+  def setup
+    @iter = ModelIterator.new Model,
+      'unknown = ?',
+      false,
+      :redis => RedisClient.new, :start_id => 5, :limit => 10,
+      :conditions => ['public = ?', true]
+  end
+
+  def test_sets_klass
+    assert_equal Model, @iter.klass
+  end
+
+  def test_sets_current_id
+    assert_equal 5, @iter.current_id
+  end
+
+  def test_sets_conditions
+    assert_equal ['models.id > ? AND (public = ?)', @iter.current_id, true], @iter.conditions
+  end
+
+  def test_sets_limit
+    assert_equal 10, @iter.limit
+  end
+end
